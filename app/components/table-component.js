f84d57c18f3df177a3f6032f17de8dc5f85a1b8e
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-02-19 19:49:20 +0200

    master: changes default sort by firstName

--- app/components/table-component.js
+++ app/components/table-component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   sortedEntries: Ember.computed.sort('theEntries', 'sortDefinition'),
-  sortBy: 'age',
+  sortBy: 'firstName',
   reverseSort: false,
   sortDefinition: Ember.computed('sortBy', 'reverseSort', function () {
     let sortOrder = this.get('reverseSort') ? 'desc' : 'asc';
